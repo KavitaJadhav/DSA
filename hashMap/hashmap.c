e0fdfb5f7bebbbc316d94e89d3bdb483fd99233d
unknown <kavitama@Inkavitama.corporate.thoughtworks.com>
2013-12-31 12:30:10 +0530

    refactered key function

--- hashMap/hashmap.c
+++ hashMap/hashmap.c
@@ -3,36 +3,19 @@
 
 const int capacity = 10;
 
-
-// void print(HashMap* map){
-// 	int i ;
-// 	for (i= 0; i < 10; ++i){
-// 		printf("%p\n", (List*)(map->buckets+(sizeof(List)*i)));
-// 	}
-// };
-
 HashMap* create(HashFunc hashFunc, CompareFunc compare){
 	int i;
 	HashMap* map = calloc(1, sizeof(HashMap));
 	map->buckets = calloc(capacity, sizeof(List));
-	for(i=0;i<capacity;i++){
-		map->buckets[i] = createList();
-	}
+	for(i=0;i<capacity;i++) 	map->buckets[i] = createList();
 	map->noOfBuckets = capacity;
 	map->hashFunc = hashFunc;
 	map->compare = compare;
 	return map;
 };
 
-Node* createNode(void* hashElement){
-	Node* node = calloc(1, sizeof(Node));
-	node->next = NULL;
-	node->prev = NULL;
-	node->data = hashElement;
-	return node;
-}
 HashElement* gethashElement(void *key, void *value){
-	HashElement * hashElement = calloc(1, sizeof(hashElement));
+	HashElement* hashElement = calloc(1, sizeof(hashElement));
 	hashElement->key =key;
 	hashElement->value = value;
 	return hashElement;
@@ -40,17 +23,16 @@ HashElement* gethashElement(void *key, void *value){
 List* getBucket(HashMap* map , int bucketNumber){
 	return (List*)(map->buckets[bucketNumber]);
 }
+
 int put(HashMap *map, void *key, void *value){
-	HashElement* hashElement = gethashElement(key, value);
     int bucketNumber = map->hashFunc(key , capacity) ;    
     List* list = getBucket(map , bucketNumber);
-	return insertNode(list, list->length,hashElement);
+	return insertNode(list, list->length,gethashElement(key, value));
 }
 void dispose(HashMap *map){
 	free(map->buckets);
 	free(map);
 }
-
 int searchByKey(HashMap* map , void* key){
     int bucketNumber = map->hashFunc(key , capacity) ;
     List* list = getBucket(map , bucketNumber);
@@ -87,6 +69,7 @@ int remove(HashMap *map, void *key){
 	int index  = getIndexInBucket( map ,key ,list);
 	return deleteNode(list,index);
 }
+
 Iterator keys(HashMap *map){
 	List* list = createList();
 	List* bucket;
@@ -95,7 +78,6 @@ Iterator keys(HashMap *map){
 	int i;
 	for(i = 0 ; i < capacity ; i++){
 		bucket = (List*)map->buckets[i];
-		// printf("%d\n",bucket->header!=NULL );
 		if(bucket->header != NULL){
 			it = getIterator(bucket);
 			while(it.hasNext(&it)){
@@ -107,23 +89,25 @@ Iterator keys(HashMap *map){
 	it = getIterator(list);
 	return it;
 }
-// Iterator keys(HashMap *map){
-// 	List* list = createList();
-// 	List* bucket;
-// 	HashElement* hashElement;
-// 	Iterator it;
-// 	int i;
-// 	for(i = 0 ; i < capacity ; i++){
-// 		bucket =  (List*)(map->buckets+(sizeof(List)*i));
-// 		if(bucket->header!=NULL){
-// 		it = getIterator(bucket);
-// 		while(it.hasNext(&it)){
-// 			hashElement = (HashElement*) it.next(&it);
-// 			insertNode(list, list->length, hashElement);
-// 		}
-// 		}
-// 	}
-// 	printf("%dlength",list->length);
-// 	it = getIterator(list);
-// 	return it;
-// }
+
+void rehash(HashMap *map){
+	List* list = createList();
+	List* bucket;
+	HashElement* hashElement;
+	Iterator it;
+	int i;
+	for(i = 0 ; i < capacity ; i++){
+		bucket = (List*)map->buckets[i];
+		if(bucket->header != NULL){
+			it = getIterator(bucket);
+			while(it.hasNext(&it)){
+				hashElement = (HashElement*) it.next(&it);
+				insertNode(list, list->length, hashElement->key);
+			}
+		}
+	}
+	// it = getIterator(list);
+	// return it;
+}
+
+
