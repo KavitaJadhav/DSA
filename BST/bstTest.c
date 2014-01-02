89c98f122c1eb9b14c60094b412b053511a60552
unknown <kavitama@Inkavitama.corporate.thoughtworks.com>
2014-01-02 15:17:17 +0530

    inserted right and left child to root node

--- BST/bstTest.c
+++ BST/bstTest.c
@@ -13,3 +13,23 @@ void test_must_insert_integer_in_binary_tree(){
 	ASSERT(1 == insertInBST(tree,&data));
 	ASSERT(1 == searchInBST(tree,&data));
 }
+
+void test_must_insert_large_data_on_right_side_of_root(){
+	BST* tree;
+	int data[] = {5,10};
+	tree = createBST(compare);
+	
+	ASSERT(1 == insertInBST(tree,&data[0]));
+	ASSERT(1 == insertInBST(tree,&data[1]));
+	ASSERT(1 == searchInBST(tree,&data[1]));
+}
+
+void test_must_insert_small_data_on_left_side_of_root(){
+	BST* tree;
+	int data[] = {20,10};
+	tree = createBST(compare);
+	
+	ASSERT(1 == insertInBST(tree,&data[0]));
+	ASSERT(1 == insertInBST(tree,&data[1]));
+	ASSERT(1 == searchInBST(tree,&data[1]));
+}
